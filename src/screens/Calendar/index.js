136adcdca7f2ce2ef448b5f1cf18141cbe031795
inaemon <github32377@gmail.com>
2024-07-30 09:51:45 +0900

    Update the Calendar page

--- src/screens/Calendar/index.js
+++ src/screens/Calendar/index.js
@@ -1,15 +1,26 @@
 
 import {Calendar} from "antd"
+import {useState} from "react";
 import {useNavigate} from "react-router-dom";
 import Navbar from '../../component/NavBar'; // 네비게이션 바 컴포넌트 임포트
 import Header from '../../component/Header';
 import './styles.css';
 
 function MyCalendar() {
+    const [userInteraction, setUserInteraction] = useState(false);
+    const onPanelChange = (date, mode) => {
+        setUserInteraction(false);
+    };
+
     const navigate = useNavigate();
     const onSelect = (date) => {
-        const formattedDate = date.format('YYYY-MM-DD');
-        navigate(`/schedule`, { state: { date: formattedDate } });
+        // 날짜 셀 클릭일 때만 이벤트 발생
+        if (date && date.format && userInteraction) {
+            const formattedDate = date.format('YYYY-MM-DD');
+            navigate(`/schedule`, { state: { date: formattedDate } });
+        }
+        // 초기화
+        setUserInteraction(false);
     };
 
 
@@ -19,7 +30,14 @@ function MyCalendar() {
         <div>
             <Header/>
             <Navbar/>
-            <Calendar onSelect={onSelect}/>
+            <Calendar 
+                onPanelChange={onPanelChange}
+                onSelect={(date) => {
+                    onSelect(date);
+                    // 날짜 셀 클릭 시만 selectedDate를 설정
+                    setUserInteraction(true);
+                }}
+            />
         </div>
     );
 }
